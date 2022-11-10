64ccbab0fe483d8f86148bc06c520a96c7fa228e
wesa5 <wobulufrancis@gmail.com>
2022-11-10 19:41:05 +0300

    fabric and nginx

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,40 +1,31 @@
 #!/usr/bin/python3
-""" Function that compress a folder """
+"""
+Fabric script that generates deploys an archive to the web
+servers
+"""
+from fabric.api import run, put, env
 from datetime import datetime
-from fabric.api import *
-import shlex
-import os
+from os.path import isfile, basename
 
-
-env.hosts = ['44.211.97.124', '34.228.52.136']
-env.user = "ubuntu"
+env.hosts = ["18.207.159.210", "44.200.65.178"]
 
 
 def do_deploy(archive_path):
-    """ Deploys """
-    if not os.path.exists(archive_path):
+    """distributes an archive to your web servers"""
+    if not isfile(archive_path):
         return False
+    filename = basename(archive_path)
     try:
-        name = archive_path.replace('/', ' ')
-        name = shlex.split(name)
-        name = name[-1]
-
-        wname = name.replace('.', ' ')
-        wname = shlex.split(wname)
-        wname = wname[0]
-
-        releases_path = "/data/web_static/releases/{}/".format(wname)
-        tmp_path = "/tmp/{}".format(name)
-
+        no_ext = filename.split(".")[0]
         put(archive_path, "/tmp/")
-        run("mkdir -p {}".format(releases_path))
-        run("tar -xzf {} -C {}".format(tmp_path, releases_path))
-        run("rm {}".format(tmp_path))
-        run("mv {}web_static/* {}".format(releases_path, releases_path))
-        run("rm -rf {}web_static".format(releases_path))
+        extract_path = "/data/web_static/releases/{}".format(no_ext)
+        run("mkdir -p {}".format(extract_path))
+        run("tar xzf /tmp/{} -C {}".format(filename, extract_path))
+        run("rm /tmp/{}".format(filename))
+        run("mv {0}/web_static/* {0}/".format(extract_path))
+        run("rm -rf {0}/web_static/".format(extract_path))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(releases_path))
-        print("New version deployed!")
+        run("ln -s {} /data/web_static/current".format(extract_path))
         return True
     except:
         return False
